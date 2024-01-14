d7af849083b3841957a7cab88162ba01374fb19c
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-15 02:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function firstWord(s) {
   // your code here
+	if(s.length==0){
+		return "";
+	}
+	// let n=s.length;
+	// let c=0;
+	// for(let i=0;i<n;i++){
+	// 	if(s.charAt(i)==0){
+	// 		c++;
+	// 		break;
+	// 	}
+	// }
+	// if(c!=0){
+	// 	return s;
+	// }
+	let arr=s.split(" ");
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
